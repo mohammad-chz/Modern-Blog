a51b7a4d33660a8c992fa39f4d1eb29e0ea103bc
Mohammad Cheraghzadeh <151393192+mohammad-chz@users.noreply.github.com>
2024-06-23 14:53:52 +0330

    create header

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -5,9 +5,11 @@ import About from './pages/About'
 import SignIn from './pages/SignIn'
 import SignUp from './pages/SignUp'
 import Projects from './pages/Projects'
+import Header from './components/Header'
 const App = () => {
   return (
     <BrowserRouter>
+    <Header />
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/about' element={<About />} />
---
+++ Client/src/components/Header.jsx
@@ -0,0 +1,57 @@
+import React from 'react'
+import { Button, Navbar, TextInput } from 'flowbite-react'
+import { Link, useLocation } from 'react-router-dom'
+import { AiOutlineSearch } from 'react-icons/ai'
+import { FaMoon } from 'react-icons/fa'
+const Header = () => {
+    const path = useLocation().pathname;
+    return (
+        <Navbar className='border-b-2'>
+            <Navbar.Toggle />
+            <Link to='/' className='self-center whitespace-nowrap text-sm sm:text-xl font-bold dark:text-white'>
+                <span className='px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white'>مشاور</span>
+                چراغ زاده
+            </Link>
+            <form>
+                <TextInput
+                    type='text'
+                    placeholder='سرچ کنید'
+                    rightIcon={AiOutlineSearch}
+                    className='hidden lg:inline'
+                />
+            </form>
+            <Button className='lg:hidden w-12' color='gray' pill>
+                <AiOutlineSearch />
+            </Button>
+            <div className='flex gap-2 md:order-2'>
+                <Button className='w-12 hidden sm:inline' color="gray" pill>
+                    <FaMoon />
+                </Button>
+                <Link to='/sign-in'>
+                    <Button gradientDuoTone='purpleToBlue'>
+                        ورود
+                    </Button>
+                </Link>
+            </div>
+            <Navbar.Collapse>
+                <Navbar.Link className='ml-8' active={path === '/'} as={'div'}>
+                    <Link to='./' className='font-bold'>
+                        خانه
+                    </Link>
+                </Navbar.Link>
+                <Navbar.Link active={path === '/about'} as={'div'}>
+                    <Link to='./about' className='font-bold'>
+                        درباره ما
+                    </Link>
+                </Navbar.Link>
+                <Navbar.Link active={path === '/projects'} as={'div'}>
+                    <Link to='./projects' className='font-bold'>
+                        پروژه ها
+                    </Link>
+                </Navbar.Link>
+            </Navbar.Collapse>
+        </Navbar>
+    )
+}
+
+export default Header
