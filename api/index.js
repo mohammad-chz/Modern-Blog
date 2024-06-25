09874e2a96fadfcf92f82ab279816e9c37a7795f
Mohammad Cheraghzadeh <151393192+mohammad-chz@users.noreply.github.com>
2024-06-25 14:06:12 +0330

    create & add middleWare

--- api/index.js
+++ api/index.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import userRoutes from './routes/user.routes.js'
+import authRoutes from './routes/auth.route.js'
 
 dotenv.config();
 
@@ -12,6 +14,21 @@ mongoose.connect(process.env.MONGO).then(() =>{
 
 const app = express();
 
+app.use(express.json());
+
 app.listen(3000, () =>{
     console.log('Server is running on Port 3000!')
 });
+
+app.use('/api/user', userRoutes);
+app.use('/api/auth', authRoutes);
+
+app.use((err, req, res, next) => {
+     const statusCode = err.statusCode || 500;
+     const message = err.message || 'Internal Server Error';
+     res.status(statusCode).json({
+        success: false,
+        statusCode,
+        message, 
+     });
+})
