3d1fc4a510f315ab45207b12e31ab53bba8e46f4
Mohammad Cheraghzadeh <151393192+mohammad-chz@users.noreply.github.com>
2024-07-04 00:29:19 +0330

    darl mode provider

--- Client/src/redux/store.js
+++ Client/src/redux/store.js
@@ -1,23 +1,25 @@
 import { configureStore, combineReducers } from '@reduxjs/toolkit';
 import userReducer from './user/userSlice';
-import { persistReducer, persistStore } from 'redux-persist'
+import themeReducer from './theme/themeSlice';
+import { persistReducer, persistStore } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
 
 
 const rootReducer = combineReducers({
     user: userReducer,
+    theme: themeReducer
 });
 
-const persestConfiq = {
+const persistConfig  = {
     key: 'root',
     storage,
     version: 1,
 };
 
-const PersistedReducer = persistReducer(persestConfiq, rootReducer)
+const persistedReducer = persistReducer(persistConfig, rootReducer)
 
 export const store = configureStore({
-    reducer: PersistedReducer,
+    reducer: persistedReducer,
     middleware: ((getDefaultMiddleware) =>
         getDefaultMiddleware({ serializableCheck: false }))
 })
