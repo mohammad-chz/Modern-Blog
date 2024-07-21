95f3e7ba9a9ef47f2b6e248ec3551019e3143143
Mohammad Cheraghzadeh <151393192+mohammad-chz@users.noreply.github.com>
2024-07-21 20:36:28 +0330

    add show more functioanlity

--- Client/src/components/DashPost.jsx
+++ Client/src/components/DashPost.jsx
@@ -6,6 +6,23 @@ import { Link } from 'react-router-dom';
 const DashPost = () => {
   const { currentUser } = useSelector((state) => state.user);
   const [userPosts, setUserPosts] = useState([]);
+  const [showMore, setShowMore] = useState(true);
+
+  const handleShowMore = async() =>{
+    const startIndex = userPosts.length;
+    try {
+      const res = await fetch(`/api/post/getposts?userId=${currentUser._id}&startIndex=${startIndex}`);
+      const data = await res.json();
+      if(res.ok){
+        setUserPosts((prev) => [...prev, ...data.posts]);
+      };
+      if(data.posts.length === 0){
+        setShowMore(false);
+      }
+    } catch (error) {
+        console.log(error.message);
+    }
+  };
 
   useEffect(() => {
     const fetchPosts = async () => {
@@ -14,6 +31,9 @@ const DashPost = () => {
         const data = await res.json();
         if (res.ok) {
           setUserPosts(data.posts);
+          if (data.posts.length < 9) {
+            setShowMore(false);
+          }
         };
       } catch (error) {
         console.log(error.message);
@@ -23,6 +43,7 @@ const DashPost = () => {
       fetchPosts();
     }
   }, [currentUser._id])
+
   return (
     <div className='table-auto overflow-x-scroll md:mx-auto p-3 scrollbar scrollbar-track-slate-100 scrollbar-thumb-slate-300 dark:scrollbar-track-slate-700 dark:scrollbar-thumb-slate-500'>
       {currentUser.isAdmin && userPosts.length > 0 ? (
@@ -67,6 +88,11 @@ const DashPost = () => {
               </Table.Body>
             ))}
           </Table>
+          {showMore && (
+            <button onClick={handleShowMore} className='w-full text-teal-500 self-center text-sm py-7'>
+              موارد بشتر
+            </button>
+          )}
         </>
       ) : (
         <p>شما هنوز پستی ندارید</p>
