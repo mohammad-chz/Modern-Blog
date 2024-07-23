23721804cbebbecf1e928c560fe5cee4e9772c80
Mohammad Cheraghzadeh <151393192+mohammad-chz@users.noreply.github.com>
2024-07-23 16:02:06 +0330

    create get users api route

--- api/routes/user.routes.js
+++ api/routes/user.routes.js
@@ -1,11 +1,12 @@
 import express from 'express';
-import { deleteUser, signout, test, UpdateUser } from '../controllers/user.controller.js'
+import { deleteUser, getUsers, signout, test, UpdateUser } from '../controllers/user.controller.js'
 import { veryfyToken } from '../utils/verifyUser.js';
 const router = express.Router();
 
 router.get('/test', test);
 router.put('/update/:userId', veryfyToken, UpdateUser);
 router.delete('/delete/:userId', veryfyToken, deleteUser);
-router.post('/signout', signout); 
+router.post('/signout', signout);
+router.get('/getusers', veryfyToken, getUsers) 
 
 export default router;
