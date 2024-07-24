2b84f9b1184c600f5b104f032e1f50d5f1a7f889
Mohammad Cheraghzadeh <151393192+mohammad-chz@users.noreply.github.com>
2024-07-24 22:44:52 +0330

    delete for admin's users

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -51,16 +51,16 @@ export const UpdateUser = async (req, res, next) => {
 };
 
 export const deleteUser = async (req, res, next) => {
-  if (req.user.id !== req.params.userId) {
-    return next(errorHandler(403, 'You are not allowed to delete this user'))
+  if (!req.user.isAdmin && req.user.id !== req.params.userId) {
+    return next(errorHandler(403, 'You are not allowed to delete this user'));
   }
   try {
     await User.findByIdAndDelete(req.params.userId);
-    return res.status(200).json('User has been deleted')
+    res.status(200).json('User has been deleted');
   } catch (error) {
     next(error);
   }
-}
+};
 
 export const signout = async (req, res, next) => {
   try {
