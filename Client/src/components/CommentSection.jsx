f5701309564effa73a9b995d2cadf30b9ead7023
Mohammad Cheraghzadeh <151393192+mohammad-chz@users.noreply.github.com>
2024-07-28 17:14:53 +0330

    edits comments

--- Client/src/components/CommentSection.jsx
+++ Client/src/components/CommentSection.jsx
@@ -41,7 +41,7 @@ const CommentSection = ({ postId }) => {
                     setComments(data);
                 }
             } catch (error) {
-                console.log(error);
+                console.log(error.message);
             }
         };
         getComments();
